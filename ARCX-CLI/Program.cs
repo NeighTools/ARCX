706cf4ae334da3c4556346412acb5521871b14fb
Bepis <36346617+bbepis@users.noreply.github.com>
2018-06-17 02:57:39 +1000

    Improve argument handling in CLI tool

--- ARCX-CLI/Program.cs
+++ ARCX-CLI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using ARCX.Core.Archive;
 using ARCX.Core.Writers;
 
 namespace ARCX_CLI
@@ -12,22 +13,69 @@ namespace ARCX_CLI
 		{
 			args[0] = args[0].ToLowerInvariant();
 
+			Dictionary<string, string> arguments = new Dictionary<string, string>();
+			List<string> additionalArgs = new List<string>();
+
+			for (int i = 1; i < args.Length; i++)
+			{
+				if (!args[i].StartsWith("-")
+					&& !args[i].StartsWith("--")
+				    && !args[i].StartsWith("/"))
+				{
+					additionalArgs.AddRange(args.Skip(i));
+					break;
+				}
+
+				string key;
+
+				if (args[i].StartsWith("--"))
+					key = args[i].Substring(2);
+				else
+					key = args[i].Substring(1);
+
+				arguments.Add(key, args[++i]);
+			}
+
 			if (args[0].Contains('c'))
 			{
-				Compress(args);
+				Compress(arguments, additionalArgs);
 			}
 		}
 
-		static void Compress(string[] args)
+		static void Compress(Dictionary<string, string> arguments, List<string> additionalArgs)
 		{
+			CompressionType type = ArcXWriterSettings.DefaultSettings.CompressionType;
+
+			if (arguments.ContainsKey("c:type"))
+				switch (arguments["c:type"].ToLowerInvariant())
+				{
+					case "lz4":
+						type = CompressionType.LZ4;
+						break;
+					case "zstd":
+					default:
+						type = CompressionType.Zstd;
+						break;
+					case "brotli":
+						type = CompressionType.Brotli;
+						break;
+					case "lzham":
+						type = CompressionType.LZHAM;
+						break;
+					case "lzma":
+						type = CompressionType.LZMA;
+						break;
+				}
+
 			ArcXWriter writer = new ArcXWriter(new ArcXWriterSettings
 			{
-				CompressionLevel = args[0].Contains('h') ? 10 : 0
+				CompressionLevel = arguments.ContainsKey("c:level") ? int.Parse(arguments["c:level"]) : 0,
+				CompressionType = type,
 			});
 
 			List<Tuple<string, string>> filesList = new List<Tuple<string, string>>();
 
-			foreach (string arg in args.Skip(2))
+			foreach (string arg in additionalArgs.Skip(1))
 			{
 				if (File.Exists(arg))
 				{
@@ -45,7 +93,7 @@ namespace ARCX_CLI
 				writer.AddFile(new ArcXWriterFile(file.Item1, () => File.OpenRead(file.Item2)));
 			}
 
-			writer.Write(File.Open(args[1], FileMode.Create));
+			writer.Write(File.Open(additionalArgs[0], FileMode.Create));
 		}
 	}
 }
