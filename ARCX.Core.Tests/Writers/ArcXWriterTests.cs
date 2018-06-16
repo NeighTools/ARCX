fdc6ae6e33004859dc43991f0c670dde71b379aa
Bepis <36346617+bbepis@users.noreply.github.com>
2018-06-16 15:20:40 +1000

    Add basic archive test

---
+++ ARCX.Core.Tests/Writers/ArcXWriterTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ARCX.Core.Writers;
+using System.IO;
+using System.Text;
+using ARCX.Core.Archive;
+
+namespace ARCX.Core.Tests.Writers
+{
+	[TestClass]
+	public class ArcXWriterTests
+	{
+		[TestMethod]
+		public void WriteTest()
+		{
+			var writer = new ArcXWriter();
+
+			writer.AddFile(new ArcXWriterFile("dir/testfilename", () => new MemoryStream(Encoding.ASCII.GetBytes("testdata"))));
+
+			using (MemoryStream ms = new MemoryStream())
+			{
+				writer.Write(ms, true);
+
+				ms.Position = 0;
+
+				ArcXContainer container = new ArcXContainer(ms);
+				var fg = container.Chunks;
+			}
+		}
+	}
+}
