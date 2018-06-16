d8a1d8489745ee92a48d0f928c06d00efd5490c9
Bepis <36346617+bbepis@users.noreply.github.com>
2018-06-17 04:26:24 +1000

    Fix missing namespace

--- ARCX.Core/Writers/ArcXWriter.cs
+++ ARCX.Core/Writers/ArcXWriter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using ARCX.Core.Archive;
 using ARCX.Core.Compressors;
 using ARCX.Core.External;
